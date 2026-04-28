6f0b0885fe1ecada44c8db25e2073fc2edb2c867
1 <15710551907@163.com>
2026-04-28 23:05:51 +0800

    Changes

---
+++ src/HoneWork4/WordNumSearch.java
@@ -0,0 +1,22 @@
+package HoneWork4;
+
+public class WordNumSearch {
+    public static void main(String[] args){
+        String Text="2020年,大连与沈阳的列车D7732/D7733 、G3725/G3728 等都暂停过，还有其他车次例如，“Z18”、“T126”、“K1313”、“L210”和1276等列车也暂停过。";
+        StringBuilder word = new StringBuilder();
+        for(int i =0;i<Text.length();i++){
+            char c=Text.charAt(i);
+            if ((c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '/'){
+                word.append(c);          //append就是用来一个一个内容往上加着写的,append("H"}append("HE"} ...
+            }else{
+                if (word.length() > 0){
+                    System.out.println(word);
+                    word.setLength(0);   //清空word
+                }
+            }
+        }
+        if (word.length()>0){
+            System.out.println(word);
+        }
+    }
+}
---
+++ src/HoneWork4/WordSearch.java
@@ -0,0 +1,22 @@
+package HoneWork4;
+
+public class WordSearch {
+    public static void main(String[] args){
+        String Text="They brought a lot of things. Mummy pig first put sunscreen on   peppa and George, and then peppa and George began to play. Play catch the   ball first, then put on the life-saving bag, swim at the seaside and spray   water on each other.";
+        StringBuilder word = new StringBuilder();
+        for(int i =0;i<Text.length();i++){
+            char c=Text.charAt(i);
+            if ((c>='a'&&c<='z' ||c>='A' && c<='Z')){
+                  word.append(c);          //append就是用来一个一个内容往上加着写的,append("H"}append("HE"} ...
+            }else{
+                if (word.length() > 0){
+                    System.out.println(word);
+                    word.setLength(0);   //清空word
+                }
+            }
+        }
+        if (word.length()>0){
+            System.out.println(word);
+        }
+    }
+}
